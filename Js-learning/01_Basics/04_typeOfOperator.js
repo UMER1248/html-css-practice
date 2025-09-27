1e8c4c5508e3cb576c3ab20b455db40a5efe09ad
Omer Farooq <86463542+UMER1248@users.noreply.github.com>
2025-09-28 00:48:23 +0500

    Create 04_typeOfOperator.js

---
+++ Js-learning/01_Basics/04_typeOfOperator.js
@@ -0,0 +1,44 @@
+// <<<=================== TYPE OF OPERATOR =====================>>>>
+
+// Typeof operator is used to find the type of a value, like Number, boolean, string, Object.
+
+console.log(typeof 123);
+
+console.log(typeof "1234");
+
+console.log(typeof true);
+
+console.log(typeof false);
+
+console.log(typeof "123abc");
+
+console.log(typeof NaN);
+
+console.log(typeof null);
+
+console.log(typeof undefined);
+
+console.log(typeof []);        // [] is object's syntax, and type also
+
+console.log(typeof (function(){}));
+
+console.log(typeof (Symbol()));
+
+console.log(typeof 1n);
+
+console.log(typeof "abcd");
+
+
+
+console.log(typeof true + 1);
+
+console.log(typeof (true + 1));
+
+console.log(typeof typeof true);
+
+console.log(typeof true + "abcd");
+
+console.log(typeof typeof false);
+
+console.log(typeof Symbol("id"));
+
